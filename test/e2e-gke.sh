69e1184b89b9d1c3aff270992c9aa3f17d8cc196
rimas <rmocius@gmail.com>
2018-09-17 13:16:44 +0300

    a fix for GKE creds

--- test/e2e-gke.sh
+++ test/e2e-gke.sh
@@ -23,18 +23,15 @@ main() {
     # shellcheck disable=SC2064
     trap "docker rm -f $config_container_id" EXIT
 
+    docker exec "$config_container_id" gcloud auth activate-service-account --key-file /gcloud-service-key.json >/dev/null 2>&1
+    docker exec "$config_container_id" gcloud container clusters get-credentials $CLUSTER_NAME --project $PROJECT_NAME --zone $CLOUDSDK_COMPUTE_ZONE
+
     # --- Work around for Tillerless Helm, till Helm v3 gets released --- #
     docker exec "$config_container_id" helm init --client-only
     docker exec "$config_container_id" helm plugin install https://github.com/rimusz/helm-tiller
     docker exec "$config_container_id" bash -c 'echo "Starting Tiller..."; helm tiller start-ci >/dev/null 2>&1 &'
     docker exec "$config_container_id" bash -c 'echo "Waiting Tiller to launch on 44134..."; while ! nc -z localhost 44134; do sleep 1; done; echo "Tiller launched..."'
     echo
-    # ------------------------------------------------------------------- #
-
-    docker exec "$config_container_id" gcloud auth activate-service-account --key-file /gcloud-service-key.json
-    docker exec "$config_container_id" gcloud container clusters get-credentials $CLUSTER_NAME --project $PROJECT_NAME --zone $CLOUDSDK_COMPUTE_ZONE
-
-    # -- Work around for Tillerless Helm, till Helm v3 gets released -- #
     docker exec -e HELM_HOST=localhost:44134 "$config_container_id" chart_test.sh --config /workdir/test/.testenv
     # ------------------------------------------------------------------- #
 
