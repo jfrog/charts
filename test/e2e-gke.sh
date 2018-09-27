4af354677f78c9060939ac941a2e6cd632f02dbb
rimas <rmocius@gmail.com>
2018-09-27 19:37:33 +0100

    run 1

--- test/e2e-gke.sh
+++ test/e2e-gke.sh
@@ -5,41 +5,43 @@ set -o nounset
 set -o pipefail
 
 readonly REPO_ROOT="${REPO_ROOT:-$(git rev-parse --show-toplevel)}"
-
-echo "Install socat and util-linux"
-sudo apt-get update
-sudo apt-get install -y openssl
-echo
+pwd
 
 # shellcheck disable=SC2086
 echo $GCLOUD_SERVICE_KEY_CHARTS_CI | base64 --decode -i > ${PWD}/gcloud-service-key.json
 # shellcheck disable=SC2086
-echo $GCLOUD_GKE_CLUSTER | base64 --decode -i > ${PWD}/cluster
+echo $GCLOUD_GKE_CLUSTER | base64 --decode -i > ${PWD}/gke_cluster
 # shellcheck disable=SC1090,SC2086
-source ${PWD}/cluster > /dev/null
+source ${PWD}/gke_cluster
 
-#
-git remote add k8s "${CHARTS_REPO}" &> /dev/null || true
-git fetch k8s master
+ls -alh
 
-config_container_id=$(docker run -ti -d -v "${PWD}/gcloud-service-key.json:/gcloud-service-key.json" -v "$REPO_ROOT:/workdir" \
-    "$TEST_IMAGE:$TEST_IMAGE_TAG" cat)
+main() {
+    git remote add k8s "${CHARTS_REPO}" &> /dev/null || true
+    git fetch k8s master
 
-# shellcheck disable=SC2064
-trap "docker rm -f $config_container_id" EXIT
+    local config_container_id
+    config_container_id=$(docker run -ti -d -v "${PWD}/gcloud-service-key.json:/gcloud-service-key.json" -v "$REPO_ROOT:/workdir" \
+        "$TEST_IMAGE:$TEST_IMAGE_TAG" cat)
 
-docker exec "$config_container_id" gcloud auth activate-service-account --key-file /gcloud-service-key.json
-docker exec "$config_container_id" gcloud container clusters get-credentials "$CLUSTER_NAME" --project "$PROJECT_NAME" --zone "$CLOUDSDK_COMPUTE_ZONE"
+    # shellcheck disable=SC2064
+    trap "docker rm -f $config_container_id" EXIT
 
-# --- Work around for Tillerless Helm, till Helm v3 gets released --- #
-docker exec "$config_container_id" helm init --client-only
-docker exec "$config_container_id" helm plugin install https://github.com/rimusz/helm-tiller
-docker exec "$config_container_id" bash -c 'echo "Starting Tiller..."; helm tiller start-ci >/dev/null 2>&1 &'
-docker exec "$config_container_id" bash -c 'echo "Waiting Tiller to launch on 44134..."; while ! nc -z localhost 44134; do sleep 1; done; echo "Tiller launched..."'
-echo
-docker exec -e HELM_HOST=localhost:44134 "$config_container_id" chart_test.sh --no-lint --config /workdir/test/.testenv
-# ------------------------------------------------------------------- #
+    docker exec "$config_container_id" gcloud auth activate-service-account --key-file /gcloud-service-key.json
+    docker exec "$config_container_id" gcloud container clusters get-credentials "$CLUSTER_NAME" --project "$PROJECT_NAME" --zone "$CLOUDSDK_COMPUTE_ZONE"
 
-##### docker exec "$config_container_id" chart_test.sh --config /workdir/test/.testenv
+    # --- Work around for Tillerless Helm, till Helm v3 gets released --- #
+    docker exec "$config_container_id" helm init --client-only
+    docker exec "$config_container_id" helm plugin install https://github.com/rimusz/helm-tiller
+    docker exec "$config_container_id" bash -c 'echo "Starting Tiller..."; helm tiller start-ci >/dev/null 2>&1 &'
+    docker exec "$config_container_id" bash -c 'echo "Waiting Tiller to launch on 44134..."; while ! nc -z localhost 44134; do sleep 1; done; echo "Tiller launched..."'
+    echo
+    docker exec -e HELM_HOST=localhost:44134 "$config_container_id" chart_test.sh --no-lint --config /workdir/test/.testenv
+    # ------------------------------------------------------------------- #
 
-echo "Done Testing!"
+    ##### docker exec "$config_container_id" chart_test.sh --config /workdir/test/.testenv
+
+    echo "Done Testing!"
+}
+
+main
