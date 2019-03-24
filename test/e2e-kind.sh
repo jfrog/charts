bb667877de52cf2420e219c596177b7cfb6e52e6
rimas <rmocius@gmail.com>
2019-03-24 19:24:18 +0000

    enable back set commands

--- test/e2e-kind.sh
+++ test/e2e-kind.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-#set -o errexit
-#set -o nounset
-#set -o pipefail
+set -o errexit
+set -o nounset
+set -o pipefail
 
 readonly REPO_ROOT="${REPO_ROOT:-$(git rev-parse --show-toplevel)}"
 readonly CLUSTER_NAME=chart-testing
