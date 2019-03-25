e074984a8c623c178bb3fe7ba8292202af58c6dd
rimas <rmocius@gmail.com>
2019-03-25 10:08:44 +0000

    refactor/update other scripts

--- test/e2e-kind.sh
+++ test/e2e-kind.sh
@@ -63,16 +63,6 @@ create_kind_cluster() {
     echo
 }
 
-install_tiller() {
-     docker_exec apk add bash
-     echo "Install Tillerless Helm plugin..."
-     docker_exec helm init --client-only
-     docker_exec helm plugin install https://github.com/rimusz/helm-tiller
-     docker_exec bash -c 'echo "Starting Tiller..."; helm tiller start-ci >/dev/null 2>&1 &'
-     docker_exec bash -c 'echo "Waiting Tiller to launch on 44134..."; while ! nc -z localhost 44134; do sleep 1; done; echo "Tiller launched..."'
-     echo
-}
-
 install_local-path-provisioner() {
     # kind doesn't support Dynamic PVC provisioning yet, this is one ways to get it working
     # https://github.com/rancher/local-path-provisioner
@@ -85,7 +75,22 @@ install_local-path-provisioner() {
     echo
 }
 
+install_tiller() {
+     docker_exec apk add bash
+     echo "Install Tillerless Helm plugin..."
+     docker_exec helm init --client-only
+     docker_exec helm plugin install https://github.com/rimusz/helm-tiller
+     docker_exec bash -c 'echo "Starting Tiller..."; helm tiller start-ci >/dev/null 2>&1 &'
+     docker_exec bash -c 'echo "Waiting Tiller to launch on 44134..."; while ! nc -z localhost 44134; do sleep 1; done; echo "Tiller launched..."'
+     echo
+}
+
 install_charts() {
+    echo "Add git remote k8s ${CHARTS_REPO}"
+    git remote add k8s "${CHARTS_REPO}" &> /dev/null || true
+    git fetch k8s master
+    echo
+    
     docker_exec ct install --config /workdir/test/ct.yaml
     echo
 }
--- test/lint-charts.sh
+++ test/lint-charts.sh
@@ -9,8 +9,10 @@ readonly IMAGE_REPOSITORY="quay.io/helmpack/chart-testing"
 readonly REPO_ROOT="${REPO_ROOT:-$(git rev-parse --show-toplevel)}"
 
 main() {
+    echo "Add git remote k8s ${CHARTS_REPO}"
     git remote add k8s "${CHARTS_REPO}" &> /dev/null || true
     git fetch k8s master
+    echo
     
     mkdir -p tmp
     docker run --rm -v "$(pwd):/workdir" --workdir /workdir "$IMAGE_REPOSITORY:$IMAGE_TAG" ct lint --config /workdir/test/ct.yaml | tee tmp/lint.log
