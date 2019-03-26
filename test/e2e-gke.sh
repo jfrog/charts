7c1375277a6bc278fc1c74084f51ebf02c91cab9
rimas <rmocius@gmail.com>
2019-03-26 12:14:40 +0000

    fix GKE CI

--- test/e2e-gke.sh
+++ test/e2e-gke.sh
@@ -7,6 +7,16 @@ set -o pipefail
 readonly REPO_ROOT="${REPO_ROOT:-$(git rev-parse --show-toplevel)}"
 readonly CLUSTER_NAME=chart-testing
 
+
+get_creds() {
+    # shellcheck disable=SC2086
+    echo $GCLOUD_SERVICE_KEY_CHARTS_CI | base64 --decode -i > $REPO_ROOT/gcloud-service-key.json
+    # shellcheck disable=SC2086
+    echo $GCLOUD_GKE_CLUSTER | base64 --decode -i > $REPO_ROOT/gke_cluster
+    # shellcheck disable=SC1090,SC2086
+    source $REPO_ROOT/gke_cluster
+}
+
 run_ct_container() {
     echo 'Running ct container...'
     docker run --rm --interactive --detach --name ct \
@@ -29,13 +39,6 @@ docker_exec() {
 }
 
 connect_to_cluster() {
-    # shellcheck disable=SC2086
-    echo $GCLOUD_SERVICE_KEY_CHARTS_CI | base64 --decode -i > $REPO_ROOT/gcloud-service-key.json
-    # shellcheck disable=SC2086
-    echo $GCLOUD_GKE_CLUSTER | base64 --decode -i > $REPO_ROOT/gke_cluster
-    # shellcheck disable=SC1090,SC2086
-    source $REPO_ROOT/gke_cluster
-
     docker_exec gcloud auth activate-service-account --key-file /gcloud-service-key.json
     docker_exec gcloud container clusters get-credentials "$CLUSTER_NAME" --project "$PROJECT_NAME" --zone "$CLOUDSDK_COMPUTE_ZONE"
 }
@@ -61,6 +64,7 @@ install_charts() {
 }
 
 main() {
+    get_creds
     run_ct_container
     trap cleanup EXIT
 
