dbcb8866740e0f180845e496fc4050944fb6eb14
Daniel Ezer <danielezer95@gmail.com>
2019-06-19 17:56:50 +0300

    Move the check to the lint-charts script

--- test/lint-charts.sh
+++ test/lint-charts.sh
@@ -8,14 +8,43 @@ readonly IMAGE_TAG=${CHART_TESTING_TAG}
 readonly IMAGE_REPOSITORY="quay.io/helmpack/chart-testing"
 readonly REPO_ROOT="${REPO_ROOT:-$(git rev-parse --show-toplevel)}"
 
-main() {
+git_fetch() {
     echo "Add git remote k8s ${CHARTS_REPO}"
     git remote add k8s "${CHARTS_REPO}" &> /dev/null || true
     git fetch k8s master
     echo
-    
+}
+
+get_changed_charts() {
+    local changed_charts=($(docker_exec ct list-changed --config /workdir/test/ct.yaml))
+    echo "${changed_charts}"
+}
+
+check_changelog_version() {
+    local changed_charts=$(get_changed_charts)
+    echo "Changed Charts: ${changed_charts}"
+    for chart_name in ${changed_charts} ; do
+        echo "Checking CHANGELOG for chart ${chart_name}"
+        local chart_version=$(grep 'version:' ${REPO_ROOT}/stable/${chart_name}/Chart.yaml)
+        ## Check that the version has an entry in the changelog
+        if grep -q "\[${chart_version}\]" ${REPO_ROOT}/stable/${chart_name}/CHANGELOG.md; then
+            echo "No CHANGELOG entry for chart ${chart_name} version ${chart_version}"
+            exit 1
+        else
+            echo "Found CHANGELOG entry for chart ${chart_name} version ${chart_version}"
+        fi
+    done
+    echo "Done CHANGELOG Check!"
+}
+
+
+main() {
+    git_fetch
+
     mkdir -p tmp
     docker run --rm -v "$(pwd):/workdir" --workdir /workdir "$IMAGE_REPOSITORY:$IMAGE_TAG" ct lint --config /workdir/test/ct.yaml | tee tmp/lint.log
+
+    check_changelog_version
 
     echo "Done Charts Linting!"
 }
