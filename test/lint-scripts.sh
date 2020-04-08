404ef44050692ee351c963a50cf769ba022ca75f
kammathavaran <kammathavaran@gmail.com>
2020-04-08 11:46:09 +0530

    INST-768 - revert and run from base folder

--- test/lint-scripts.sh
+++ test/lint-scripts.sh
@@ -4,7 +4,7 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-readonly FILES_TO_LINT=$(ls test/*.sh)
+readonly FILES_TO_LINT=$(ls "test/"*.sh)
 readonly IMAGE_REPOSITORY="koalaman/shellcheck-alpine"
 readonly SHELLCHECK_CMD="docker run --rm -v "$(pwd):/workdir" --workdir /workdir ${IMAGE_REPOSITORY} shellcheck -x"
 
